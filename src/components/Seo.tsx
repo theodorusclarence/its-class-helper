3ace25eaa38972738e1030045f463a685fa5a552
Theodorus Clarence <theodorusclarence@gmail.com>
2022-01-31 09:57:21 +0700

    feat: add open graph

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -1,14 +1,15 @@
+import { openGraph } from '@/lib/helper';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 const defaultMeta = {
   title: 'Informatics ITS Class Helper',
-  site_name: 'Informatics ITS Class Helper',
+  siteName: 'Informatics ITS Class Helper',
   description: 'Helper to check classes in Informatics ITS',
   url: 'https://its-class.theodorusclarence.com',
-  image: 'https://theodorusclarence.com/favicon/large-og.jpg',
   type: 'website',
   robots: 'follow, index',
+  image: '',
 };
 
 type SeoProps = {
@@ -23,8 +24,16 @@ export default function Seo(props: SeoProps) {
     ...props,
   };
   meta['title'] = props.templateTitle
-    ? `${props.templateTitle} | ${meta.site_name}`
+    ? `${props.templateTitle} | ${meta.siteName}`
     : meta.title;
+
+  // Use siteName if there is templateTitle
+  // but show full title if there is none
+  meta['image'] = openGraph({
+    description: meta.description,
+    siteName: props.templateTitle ? meta.siteName : meta.title,
+    templateTitle: props.templateTitle,
+  });
 
   return (
     <Head>
@@ -35,7 +44,7 @@ export default function Seo(props: SeoProps) {
       <link rel='canonical' href={`${meta.url}${router.asPath}`} />
       {/* Open Graph */}
       <meta property='og:type' content={meta.type} />
-      <meta property='og:site_name' content={meta.site_name} />
+      <meta property='og:site_name' content={meta.siteName} />
       <meta property='og:description' content={meta.description} />
       <meta property='og:title' content={meta.title} />
       <meta name='image' property='og:image' content={meta.image} />
--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,3 +1,31 @@
 export function classNames(...classes: string[]): string {
   return classes.filter(Boolean).join(' ');
 }
+
+type OpenGraphType = {
+  siteName: string;
+  description: string;
+  templateTitle?: string;
+  logo?: string;
+};
+// !STARTERCONF This OG is generated from https://github.com/theodorusclarence/og
+// Please clone them and self-host if your site is going to be visited by many people.
+// Then change the url and the default logo.
+export function openGraph({
+  siteName,
+  templateTitle,
+  description,
+  // !STARTERCONF Or, you can use my server with your own logo.
+  logo = 'https://www.its.ac.id/wp-content/uploads/2020/07/Lambang-ITS-2-300x300.png',
+}: OpenGraphType): string {
+  const ogLogo = encodeURIComponent(logo);
+  const ogSiteName = encodeURIComponent(siteName.trim());
+  const ogTemplateTitle = templateTitle
+    ? encodeURIComponent(templateTitle.trim())
+    : undefined;
+  const ogDesc = encodeURIComponent(description.trim());
+
+  return `https://og.thcl.dev/api/general?siteName=${ogSiteName}&description=${ogDesc}&logo=${ogLogo}${
+    ogTemplateTitle ? `&templateTitle=${ogTemplateTitle}` : ''
+  }`;
+}
