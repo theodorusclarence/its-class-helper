83bfb474fc6be207ccb0b50d235aefc6e2c79426
Theodorus Clarence <theodorusclarence@gmail.com>
2022-02-02 14:28:27 +0700

    feat: add full mk

--- src/data/matkul.ts
+++ src/data/matkul.ts
@@ -6,124 +6,104 @@ export type DataMatkul = {
 };
 
 export const dataMatkul: Array<DataMatkul> = [
+  //#region  //*=========== Semester 1 ===========
   { mkjur: '51100', sem: '1', kode: 'IF4101', nama: 'Dasar Pemrograman' },
+  //#endregion  //*======== Semester 1 ===========
+
+  //#region  //*=========== Semester 2 ===========
   { mkjur: '51100', sem: '2', kode: 'IF4201', nama: 'Sistem Digital' },
   { mkjur: '51100', sem: '2', kode: 'IF4202', nama: 'Struktur Data' },
-  {
-    mkjur: '51100',
-    sem: '3',
-    kode: 'IF4301',
-    nama: 'Pemrograman Berorientasi Objek',
-  },
+  //#endregion  //*======== Semester 2 ===========
+
+  //#region  //*=========== Semester 3 ===========
+  { mkjur: '51100', sem: '3', kode: 'IF4301', nama: 'Pemrograman Berorientasi Objek' },
   { mkjur: '51100', sem: '3', kode: 'IF4302', nama: 'Aljabar Linier' },
   { mkjur: '51100', sem: '3', kode: 'IF4303', nama: 'Komputasi Numerik' },
   { mkjur: '51100', sem: '3', kode: 'IF4304', nama: 'Matematika Diskrit' },
   { mkjur: '51100', sem: '3', kode: 'IF4305', nama: 'Organisasi Komputer' },
   { mkjur: '51100', sem: '3', kode: 'IW4301', nama: 'Sistem Basis Data' },
-  {
-    mkjur: '51100',
-    sem: '4',
-    kode: 'IF4401',
-    nama: 'Perancangan dan Analisis Algoritma',
-  },
-  {
-    mkjur: '51100',
-    sem: '4',
-    kode: 'IF4405',
-    nama: 'Probabilitas dan Statistik',
-  },
+  //#endregion  //*======== Semester 3 ===========
+
+  //#region  //*=========== Semester 4 ===========
+  { mkjur: '51100', sem: '4', kode: 'IF4401', nama: 'Perancangan dan Analisis Algoritma' },
+  { mkjur: '51100', sem: '4', kode: 'IF4405', nama: 'Probabilitas dan Statistik' },
   { mkjur: '51100', sem: '4', kode: 'IF4402', nama: 'Sistem Operasi' },
   { mkjur: '51100', sem: '4', kode: 'IF4403', nama: 'Kecerdasan Buatan' },
   { mkjur: '51100', sem: '4', kode: 'IF4404', nama: 'Manajemen Basis Data' },
-  {
-    mkjur: '51100',
-    sem: '4',
-    kode: 'IF4406',
-    nama: 'Analisis dan Perancangan Sistem Informasi',
-  },
-  {
-    mkjur: '51100',
-    sem: '5',
-    kode: 'IF4501',
-    nama: 'Perancangan Perangkat Lunak',
-  },
+  { mkjur: '51100', sem: '4', kode: 'IF4406', nama: 'Analisis dan Perancangan Sistem Informasi' },
+  //#endregion  //*======== Semester 4 ===========
+
+  //#region  //*=========== Semester 5 ===========
+  { mkjur: '51100', sem: '5', kode: 'IF4501', nama: 'Perancangan Perangkat Lunak' },
   { mkjur: '51100', sem: '5', kode: 'IF4502', nama: 'Grafika Komputer' },
-  {
-    mkjur: '51100',
-    sem: '5',
-    kode: 'IF4503',
-    nama: 'Kecerdasan Komputasional',
-  },
+  { mkjur: '51100', sem: '5', kode: 'IF4503', nama: 'Kecerdasan Komputasional' },
   { mkjur: '51100', sem: '5', kode: 'IF4504', nama: 'Pemrograman Web' },
   { mkjur: '51100', sem: '5', kode: 'IF4505', nama: 'Jaringan Komputer' },
-  {
-    mkjur: '51100',
-    sem: '5',
-    kode: 'IF4506',
-    nama: 'Manajemen Proyek Perangkat Lunak',
-  },
-  {
-    mkjur: '51100',
-    sem: '6',
-    kode: 'IF4602',
-    nama: 'Pemrograman Jaringan',
-  },
-  {
-    mkjur: '51100',
-    sem: '6',
-    kode: 'IF4603',
-    nama: 'Rekayasa Kebutuhan',
-  },
-  {
-    mkjur: '51100',
-    sem: '6',
-    kode: 'IF4952',
-    nama: 'Pengolahan Citra Digital',
-  },
-  {
-    mkjur: '51100',
-    sem: '6',
-    kode: 'IF4922',
-    nama: 'Analisis Data Multivariat',
-  },
-  {
-    mkjur: '51100',
-    sem: '6',
-    kode: 'IF4605',
-    nama: 'Pemrograman Berbasis Kerangka Kerja',
-  },
-  {
-    mkjur: '51100',
-    sem: '6',
-    kode: 'IF4601',
-    nama: 'Interaksi Manusia dan Komputer',
-  },
-  {
-    mkjur: '51100',
-    sem: '6',
-    kode: 'IF4604',
-    nama: 'Teori Graf dan Otomata',
-  },
-  {
-    mkjur: '51100',
-    sem: '6',
-    kode: 'IF4902',
-    nama: 'Pengembangan Analisis Algoritma',
-  },
-  {
-    mkjur: '51100',
-    sem: '7',
-    kode: 'IF4701',
-    nama: 'Keamanan Informasi dan Jaringan',
-  },
+  { mkjur: '51100', sem: '5', kode: 'IF4506', nama: 'Manajemen Proyek Perangkat Lunak' },
+  //#endregion  //*======== Semester 5 ===========
+
+  //#region  //*=========== Semester 6 ===========
+  { mkjur: '51100', sem: '6', kode: 'IF4605', nama: 'Pemrograman Berbasis Kerangka Kerja' },
+  { mkjur: '51100', sem: '6', kode: 'IF4902', nama: 'Pengembangan Analisis Algoritma' },
+  { mkjur: '51100', sem: '6', kode: 'IF4604', nama: 'Teori Graf dan Otomata' },
+  { mkjur: '51100', sem: '6', kode: 'IF4922', nama: 'Analisis Data Multivariat' },
+  { mkjur: '51100', sem: '6', kode: 'IF4601', nama: 'Interaksi Manusia dan Komputer' },
+  { mkjur: '51100', sem: '6', kode: 'IF4935', nama: 'Game Cerdas' },
+  { mkjur: '51100', sem: '6', kode: 'IF4602', nama: 'Pemrograman Jaringan' },
+  { mkjur: '51100', sem: '6', kode: 'IF4951', nama: 'Data Mining' },
+  { mkjur: '51100', sem: '6', kode: 'IF4952', nama: 'Pengolahan Citra Digital' },
+  { mkjur: '51100', sem: '6', kode: 'IF4971', nama: 'Arsitektur Perangkat Lunak' },
+  //#endregion  //*======== Semester 6 ===========
+
+  //#region  //*=========== Semester 7 ===========
+  { mkjur: '51100', sem: '7', kode: 'IF4911', nama: 'Jaringan Nirkabel' },
+  { mkjur: '51100', sem: '7', kode: 'IF4912', nama: 'Teknologi antar Jaringan' },
+  { mkjur: '51100', sem: '7', kode: 'IF4901', nama: 'Pemrograman Perangkat Bergerak' },
+  { mkjur: '51100', sem: '7', kode: 'IF4903', nama: 'Pemrograman Berbasis Antarmuka' },
+  { mkjur: '51100', sem: '7', kode: 'IF4923', nama: 'Riset Operasi' },
   { mkjur: '51100', sem: '7', kode: 'IF4702', nama: 'Pra-Tugas Akhir' },
+  { mkjur: '51100', sem: '7', kode: 'IF4982', nama: 'Pengantar Logika dan Pemrograman' },
+  { mkjur: '51100', sem: '7', kode: 'IF4985', nama: 'Pengantar Pengembangan Game' },
+  { mkjur: '51100', sem: '7', kode: 'IF4931', nama: 'Teknik Pengembangan Game' },
+  { mkjur: '51100', sem: '7', kode: 'IF4932', nama: 'Realitas Virtual dan Augmentasi' },
+  { mkjur: '51100', sem: '7', kode: 'IF4942', nama: 'Komputasi Awan' },
+  { mkjur: '51100', sem: '7', kode: 'IF4944', nama: 'Sistem Terdistribusi' },
+  { mkjur: '51100', sem: '7', kode: 'IF4701', nama: 'Keamanan Informasi dan Jaringan' },
+  { mkjur: '51100', sem: '7', kode: 'IF4943', nama: 'Komputasi Bergerak' },
+  { mkjur: '51100', sem: '7', kode: 'IF4948', nama: 'Kompresi Data' },
+  { mkjur: '51100', sem: '7', kode: 'IF4953', nama: 'Komputasi Biomedik' },
+  { mkjur: '51100', sem: '7', kode: 'IF4954', nama: 'Robotika' },
+  { mkjur: '51100', sem: '7', kode: 'IF4955', nama: 'Sistem Temu Kembali Informasi' },
+  { mkjur: '51100', sem: '7', kode: 'IF4956', nama: 'Visi Komputer' },
+  { mkjur: '51100', sem: '7', kode: 'IF4961', nama: 'Sistem Enterprise' },
+  { mkjur: '51100', sem: '7', kode: 'IF4962', nama: 'Rekayasa Pengetahuan' },
+  { mkjur: '51100', sem: '7', kode: 'IF4965', nama: 'Basis Data Terdistribusi' },
+  { mkjur: '51100', sem: '7', kode: 'IF4967', nama: 'Sistem Informasi Geografis' },
+  { mkjur: '51100', sem: '7', kode: 'IF4973', nama: 'Evolusi Perangkat Lunak' },
+  { mkjur: '51100', sem: '7', kode: 'IF4974', nama: 'Konstruksi Perangkat Lunak' },
+  //#endregion  //*======== Semester 7 ===========
+
+  //#region  //*=========== Semester 8 ===========
   { mkjur: '51100', sem: '8', kode: 'IF4801', nama: 'Kerja Praktik' },
-  { mkjur: '51100', sem: '8', kode: 'IF4802', nama: 'Tugas Akhir' },
+  { mkjur: '51100', sem: '8', kode: 'IF4913', nama: 'Perancangan Keamanan Sistem dan Jaringan' },
   { mkjur: '51100', sem: '8', kode: 'IF4914', nama: 'Teknologi IoT' },
-  {
-    mkjur: '51100',
-    sem: '8',
-    kode: 'IF4972',
-    nama: 'Penjaminan Mutu Perangkat Lunak',
-  },
+  { mkjur: '51100', sem: '8', kode: 'IF4921', nama: 'Pemodelan dan Simulasi' },
+  { mkjur: '51100', sem: '8', kode: 'IF4981', nama: 'Topik Khusus' },
+  { mkjur: '51100', sem: '8', kode: 'IF4986', nama: 'Pengantar Sistem Cerdas' },
+  { mkjur: '51100', sem: '8', kode: 'IF4983', nama: 'Pengantar Teknologi Basis Data' },
+  { mkjur: '51100', sem: '8', kode: 'IF4984', nama: 'Pengantar Proses Mining' },
+  { mkjur: '51100', sem: '8', kode: 'IF4933', nama: 'Sistem Game' },
+  { mkjur: '51100', sem: '8', kode: 'IF4934', nama: 'Animasi Komputer dan Pemodelan 3D' },
+  { mkjur: '51100', sem: '8', kode: 'IF4941', nama: 'Jaringan Multimedia' },
+  { mkjur: '51100', sem: '8', kode: 'IF4945', nama: 'Forensik Digital' },
+  { mkjur: '51100', sem: '8', kode: 'IF4946', nama: 'Komputasi Grid dan Paralel' },
+  { mkjur: '51100', sem: '8', kode: 'IF4947', nama: 'Komputasi Pervasif dan Jaringan Sensor' },
+  { mkjur: '51100', sem: '8', kode: 'IF4957', nama: 'Analisis Jejaring Sosial' },
+  { mkjur: '51100', sem: '8', kode: 'IF4958', nama: 'Deep Learning' },
+  { mkjur: '51100', sem: '8', kode: 'IF4963', nama: 'Audit Sistem' },
+  { mkjur: '51100', sem: '8', kode: 'IF4964', nama: 'Tata Kelola Teknologi Informasi' },
+  { mkjur: '51100', sem: '8', kode: 'IF4966', nama: 'Big Data' },
+  { mkjur: '51100', sem: '8', kode: 'IF4972', nama: 'Penjaminan Mutu Perangkat Lunak' },
+  { mkjur: '51100', sem: '8', kode: 'IF4802', nama: 'Tugas Akhir' },
+  //#endregion  //*======== Semester 8 ===========
 ];
