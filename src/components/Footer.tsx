db92dafda8a2fe7aa3a32f143cfd84f03f09fbf7
Theodorus Clarence <theodorusclarence@gmail.com>
2025-02-26 16:32:41 +0700

    feat: add tracking to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import CustomLink from './CustomLink';
+import { trackEvent } from '@/lib/analytics';
 
 export default function Footer() {
   return (
     <footer className='absolute w-full text-center text-gray-500 bottom-2'>
       © {new Date().getFullYear()} By{' '}
-      <CustomLink href='https://theodorusclarence.com?ref=tsnextstarter'>Theodorus Clarence</CustomLink>
+      <CustomLink
+        href='https://theodorusclarence.com?ref=class.thcl.dev'
+        onClick={() => {
+          trackEvent(`Click Clarence's Footer`);
+        }}
+      >
+        Theodorus Clarence
+      </CustomLink>
       {' & '}
       <CustomLink href='https://robbypambudi.com'>Robby Pambudi</CustomLink>
     </footer>
